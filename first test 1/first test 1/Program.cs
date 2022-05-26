b543cbf09464ab853dcab33b4ce549b3611a3629
AslanHasanov <aslanrh@code.edu.az>
2022-05-26 21:42:03 +0400

    first test 1

---
+++ first test 1/first test 1/Program.cs
@@ -0,0 +1,15 @@
+﻿//See https://aka.ms/new-console-template for more information
+//int mathscore = 70;
+//int englishscore = 75;
+//int chemstryscore = 80;
+
+//float totalscore = mathscore + englishscore + chemstryscore;
+
+//float overage = totalscore / 3;
+//Console.WriteLine(overage);
+
+//string name = "Aslan";
+//string surname = "Hasan";
+//int age = 23;
+
+//Console.WriteLine(name+" "+surname+" "+age);
